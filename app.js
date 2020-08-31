9af1102c92126ebe0067e3d691d13d0cfdb06af1
thewizard23 <seanpscott6@gmail.com>
2020-08-31 10:26:37 -0700

    up to the frontend

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets  = require("./routes/api/tweets");
+const User = require('./models/User');
+const bodyParser = require('body-parser');
+
+mongoose
+  .connect(db, {useNewUrlParser: true})
+  .then(() => console.log("Connnected to mongoDB"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello a/A!")
+  const user = new User({
+    handle: "johnny",
+    email: "johnny@gmail.com",
+    password: "johnny"
+  })
+  user.save();
+  res.send("Hello a/A!");
 })
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port =  process.env.PORT || 5000;
 
---
+++ routes/api/tweets.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+const validateTweetInput = require("../../validation/tweets");
+const Tweet = require("../../models/Tweet");
+const { restart } = require("nodemon");
+
+
+router.get("/test", (req, res) => {
+  res.json({ msg: "This is the tweet route" });
+});
+
+router.get("/", (req, res) => {
+  Tweet.find().sort({date: -1}).then(tweets => res.json(tweets))
+  .catch(err => res.status(400).json(err));
+})
+
+router.get("/user/:user_id", (req, res) => {
+  Tweet.find({ user: req.params.user_id })
+    .then((tweets) => res.json(tweets))
+    .catch((err) => res.status(400).json(err));
+})
+
+router.get("/:id", (req, res) => {
+  Tweet.findById(req.params.id)
+    .then((tweet) => res.json(tweet))
+    .catch((err) => res.status(400).json(err));
+})
+
+router.post("/",
+passport.authenticate("jwt", {session: false}),
+  (req,res) => {
+    const { isValid, errors } = validateTweetInput(req.body)
+
+    if (!isValid){
+      return res.status(400).json(errors);
+    }
+
+    const newTweet = new Tweet({
+      user: req.user.isValid,
+      text: req.body.text
+    })
+
+    newTweet.save().then(tweet => res.json(tweet));
+  }
+)
+
+module.exports = router;
